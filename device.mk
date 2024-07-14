2e6dea56327e407a6d23d5f79b8db09594a1923e
Hendra Manudinata <manoedinata@gmail.com>
2024-07-14 11:55:46 +0000

    j4lte: Disable LCD density override

--- device.mk
+++ device.mk
@@ -16,9 +16,6 @@
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Property overrides
--include $(LOCAL_PATH)/product_prop.mk
-
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal7570-common/universal7570-common.mk)
 
