dd5554e3c73e5f5aa7503e58ebad14db04cbb6a1
Hendra Manudinata <manoedinata@gmail.com>
2024-07-20 17:09:20 +0000

    j4lte: Set `PRODUCT_MODEL` to device name

--- omni_j4lte.mk
+++ omni_j4lte.mk
@@ -3,6 +3,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := j4lte
 PRODUCT_NAME := omni_j4lte
-PRODUCT_MODEL := SM-J400
+PRODUCT_MODEL := Galaxy J4
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
